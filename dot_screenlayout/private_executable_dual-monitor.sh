c8445c153c691d4eefcf56b1ef98bf8044eaf577
joaojr <joao.jr13@icloud.com>
2025-01-31 01:36:22 -0300

    Initial commit

---
+++ dot_screenlayout/private_executable_dual-monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 1080x240 --rate 179.96 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate left --rate 165 --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-A-2-0 --off --output DisplayPort-2-0 --off --output DisplayPort-2-1 --off --output DisplayPort-2-2 --off
